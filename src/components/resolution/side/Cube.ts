ec5a1ce28246f55832b5d3d156c72c600d8444f4
hivx <chuvanhieu357@gmail.com>
2024-11-15 22:55:12 +0700

    [update] cube

--- src/components/resolution/side/Cube.ts
+++ src/components/resolution/side/Cube.ts
@@ -1,14 +1,14 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial, BackSide } from 'three';
+import * as THREE from 'three';
 import { pano } from '@/components/resolution/config.json'
 
-function createCube(): Mesh {
+function createCube(): THREE.Mesh {
     const boxSize = pano.tileBase + pano.maxLevels + 2
-    const geometry = new BoxGeometry(boxSize, boxSize, boxSize)
-    const material = new MeshBasicMaterial( {color: 0x00ff00} )
-    material.side = BackSide
+    const geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize)
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} )
+    material.side = THREE.BackSide
     material.opacity = 1
     material.transparent = true
-    return new Mesh( geometry, material )
+    return new THREE.Mesh( geometry, material )
 }
 
 export { createCube }
